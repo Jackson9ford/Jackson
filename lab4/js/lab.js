70f43b3de3ee080c9a8246cfc118499a147913d8
Jackson9ford <jackson.ford0909@gmail.com>
2024-10-21 18:34:10 -0700

    did lab 4 and 5

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,12 +1,24 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - matching game
+// Author: Jackson Ford <jadoford@ucsc.edu>
+// Date: Oct 2024
 
-// Constants
+// Show button that says "Play Memory"
+  //Show player 20 identical rectangular cells to click on in a 4 x 5 fashion taking up the size of the screen 
+//The player clicks 2 Cells that reveal photos
+  //If the photos match, both cells disappear
+  //If the photos do not match, they turn blank again but keep their same hidden characteristics
+  //For every 2 cells picked, a number labeled “attempts” will rise by 1
+  //For each attempt in which the cards do not match, the number labeled “Mistakes” will rise by 1
+//When the player matches all pairs, the display says “CONGRATS YOU WIN”
+  //Under is a score display
+    //3 Stars appear for <5 mistakes and <15 attempts 
+    //2 stars for 5>10 mistakes and 15>20 attempts
+    //1 star for 10>15 mistakes and 20>25 attempts
+    //0 stars for >15 mistakes and  >25 attempts
+  //Underneath is a button that says "Back to Home"
 
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
+  
+  // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
   // some code here
   // return results;
--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,10 +1,22 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - lab 5
+// Author: Jackson Ford
+// Date: 10/21/2024
 
-// Constants
+// CDefine Variables
+make = "Nissan";
+model = "Maxima";
+color = "Silver";
+year = 2005;
 
-// Functions
+// Calculate
+age = 2024 - year;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
